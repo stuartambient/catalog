c43785ea1f701bc1d6a04f17ff68a9c432ffd4b8
stuartambient <sambient@gmail.com>
2020-10-21 18:49:59 -0600

    added eslint and eslint-node, including config

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const express = require("express");
+/* eslint-disable import/newline-after-import */
+const express = require('express');
 const app = express();
-const mongoist = require("mongoist");
-const musicRouter = require("./routes/musicRoutes");
+const mongoist = require('mongoist');
+const musicRouter = require('./routes/musicRoutes');
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.use(
   express.json({
-    limit: "10kb",
+    limit: '10kb',
   })
 );
-app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
-app.use("/api/v1/library", musicRouter);
+app.use('/api/v1/library', musicRouter);
 
 module.exports = app;
